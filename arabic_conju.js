15a1c5e73f91216b011f7a0554aedba024c99306
Md. Anisur Rahman <poetanis@gmail.com>
2026-02-07 13:19:30 +0600

    Update arabic_conju.js

--- arabic_conju.js
+++ arabic_conju.js
@@ -1752,7 +1752,7 @@ else if ((number1 !== "أ" && number1 !=="و" && number1 !=="ي") && (number2 !=
   document.getElementById("result").innerHTML =
     'Weak roots - أ ي و or Double letters are not contained in web. ' +
     'Our Arabic Verb Conjugator can conjugate all Arabic verbs with 100% accuracy.<br>' +
-    'Do you want to know about advanced Arabic Verb Conjugation?' + 'So you can download ' +
+    'Do you want to know about advanced Arabic Verb Conjugation?' + ' So you can download ' +
     '<a href="https://play.google.com/store/apps/details?id=com.arbiotechbd.arabicverbconjugator" target="_blank">' +
     'Arabic Verb Conjugator</a> App from Google Play Store.';
   }
