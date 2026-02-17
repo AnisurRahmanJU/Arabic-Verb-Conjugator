7e928b8196a5b8641fab21c2ee0f1344ae506318
Md. Anisur Rahman <poetanis@gmail.com>
2026-02-17 20:50:09 +0600

    Fix HTML content in error message for clarity

--- arabic_conju.js
+++ arabic_conju.js
@@ -1750,11 +1750,11 @@ else if ((number1 !== "أ" && number1 !=="و" && number1 !=="ي") && (number2 !=
   
   else {
   document.getElementById("result").innerHTML =
-    'Weak roots - أ ي و or Double letters are not contained in this webpage. ' +
+    'Weak roots - أ ي و or Double letters are not contained in this webpage.<br> ' +
     'Our System can conjugate all Arabic verbs with 100% accuracy.<br>' +
-    'Do you want to know about advanced Arabic Verb Conjugation?' + ' So you can download ' +
+    'Do you want to know about advanced Arabic Verb Conjugation?<br>'+'So you can download ' +
     '<a href="https://play.google.com/store/apps/details?id=com.arbiotechbd.arabicverbconjugator" target="_blank">' +
-    'Arabic Verb Conjugator</a> App from Google Play.';
+    'Arabic Verb Conjugator</a> App from Google Play Store.';
   }
 
 }
